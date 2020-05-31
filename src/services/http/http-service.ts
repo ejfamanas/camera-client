6ebfa9fd15c82ee1defe1a1e5e0d74a0dc4b95f9
Eric Famanas <efamanas@crfs.com>
2020-05-31 16:26:42 +0100

    start http service

--- src/services/http/http-service.ts
+++ src/services/http/http-service.ts
@@ -1,15 +1,29 @@
-import * as http from "axios";
+import {AxiosResponse, default as http} from "axios";
 
-export class HttpService {
-    private readonly _username: string;
-    private readonly _password: string;
+export class HttpService<T> {
+
     private readonly _url: string;
     private readonly _api: string;
+    private readonly _auth: { username: string, password: string };
 
     public constructor(username: string, password: string, url: string, api: string) {
-        this._username = username;
-        this._password = password;
         this._url = url;
         this._api = api;
+        this._auth = {
+            username,
+            password,
+        }
+    }
+
+    public async getAll(endpoint: string): Promise<T> {
+        return http.get(`${this._url}/${this._api}/${endpoint}`, {
+            auth: this._auth
+        }).then(({data}: AxiosResponse<T>) => data);
+    }
+
+    public async get(endpoint: string, id: number): Promise<T> {
+        return http.get(`${this._url}/${this._api}/${endpoint}/${id}`, {
+            auth: this._auth
+        }).then(({data}: AxiosResponse<T>) => data);
     }
 }
